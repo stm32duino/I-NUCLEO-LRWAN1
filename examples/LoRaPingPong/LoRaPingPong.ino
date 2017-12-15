17060e119675287d03c1e263e8cf972ed5cb00f7
fpr <fabien.perroquin@wi6labs.com>
2017-12-15 10:49:08 +0100

    Add LoRa basic example
    Signed-off-by: fpr <fabien.perroquin@wi6labs.com>

---
+++ examples/LoRaPingPong/LoRaPingPong.ino
@@ -0,0 +1,62 @@
+/*
+  LoRaPingPong.ino
+
+  Provides an example to use the shield as a simple LoRa radio.
+  Data are exchange between two boards. The first receiving the PING message
+  becomes slave and will just send a PONG answer.
+
+*/
+
+#include "LoRaRadio.h"
+
+#define SEND_PERIOD_MS 1000 //send period every second.
+
+// Serial port use to communicate with the USI shield. Specific to the board Discovery L475 IoT.
+HardwareSerial SerialLora(PA_1, PA_0);
+
+// Messages to exchange
+uint8_t PingMsg[] = "PING";
+uint8_t PongMsg[] = "PONG";
+
+bool isMaster = true;
+bool next = true;
+int timer = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("-- LoRa Ping Pong sketch --");
+
+  while(!loraRadio.begin(&SerialLora)) {
+    Serial.println("LoRa module not ready");
+    delay(1000);
+  }
+
+  Serial.println("LoRa module ready\n");
+}
+
+void loop()
+{
+  uint8_t rcvData[64];
+
+  if((isMaster == true) && (next == true)) {
+    next = false;
+    loraRadio.write(PingMsg, 4);
+    timer = millis();
+  }
+
+  if(loraRadio.read(rcvData) > 0) {
+    if(memcmp(rcvData, PongMsg, 4) == 0) {
+      Serial.println((char *)PongMsg);
+    } else if(memcmp(rcvData, PingMsg, 4) == 0) {
+      isMaster = false;
+      loraRadio.write(PongMsg, 4);
+      Serial.println((char *)PingMsg);
+    }
+    memset(rcvData, 0, 5);
+  }
+
+  if(((millis() - timer) >= SEND_PERIOD_MS) && (isMaster == true)){
+    next = true;
+  }
+}
